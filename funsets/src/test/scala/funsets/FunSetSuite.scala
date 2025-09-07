04075fcec798c2ec22a2c58e444d6cb66526b17a
limitless789 <dongwook.dc@gmail.com>
2025-09-07 18:55:29 +0900

    update funsets testcases

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -77,6 +77,8 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(4)
+    val s5 = singletonSet(5)
   }
 
   /**
@@ -86,7 +88,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet contains elem") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -98,15 +100,62 @@ class FunSetSuite extends FunSuite {
        * the test fails. This helps identifying which assertion failed.
        */
       assert(contains(s1, 1), "Singleton")
+      assert(!contains(s2, 1), "Singleton")
+      assert(contains(s2, 2), "Singleton")
+      assert(!contains(s3, 2), "Singleton")
+      assert(contains(s3, 3), "Singleton")
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test("forall with complex sets") {
+    new TestSets {
+      val setForAll = union(union(s1, s2), union(s3, s4))
+      val evens = intersect(setForAll, union(s2, s4))
+
+      assert(forall(setForAll, x => x > 0), "All elements positive")
+      assert(forall(evens, x => x % 2 == 0), "All elements of evens are even")
+      assert(!forall(setForAll, x => x % 2 == 0), "Not all elements are even")
+    }
+  }
+
+  test("exists with complex sets") {
+    new TestSets {
+      val setExists = union(union(s1, s2), union(s3, union(s4, s5)))
+
+      assert(exists(setExists, x => x % 2 == 1), "Odd exist")
+      assert(!exists(setExists, x => x > 10), "element > 10 not exists")
+      assert(exists(setExists, x => x == 3 || x == 4), "3 or 4 exists")
+      assert(!exists(setExists, x => x == 3 && x == 4), "3 also 4 not exists")
+    }
+  }
+
+  test("map with complex sets") {
+    new TestSets {
+      val setMap = union(union(s1, s2), s3)
+      val squares = map(setMap, x => x * x)
+      assert(contains(squares, 1), "Squares contains 1")
+      assert(contains(squares, 4), "Squares contains 4")
+      assert(contains(squares, 9), "Squares contains 9")
+      assert(!contains(squares, 2), "Squares not contains 2")
+
+      val shifted = map(setMap, x => x + 10)
+      assert(contains(shifted, 11), "Shifted contains 11")
+      assert(contains(shifted, 12), "Shifted contains 12")
+      assert(contains(shifted, 13), "Shifted contains 13")
+
+      val setMap2 = union(s4, s5)
+      val mod2 = map(setMap2, x => x % 2)
+      assert(contains(mod2, 0), "Mapped contains 0")
+      assert(contains(mod2, 1), "Mapped contains 1")
     }
   }
 }
